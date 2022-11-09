e7a5c1fc534ee8c0271066d65010ed18db082997
sochoall <sochoa@libelulasoft.com>
2022-11-09 12:49:47 -0500

    REFACTOR: Gridview wrapped into Scaffold since it's a screen also we're iterating the Categories here

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
 
+import './data.dart';
+import './category_item.dart';
+
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("DeliMeal"),
       ),
-      children: <Widget>[],
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children:
+            categories.map((e) => CategoryItem(e.title, e.color)).toList(),
+      ),
     );
   }
 }
