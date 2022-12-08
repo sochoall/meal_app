d47cc7c1c0e5bce4e3fbb02b241e53a5fcd61813
sochoall <sochoa@libelulasoft.com>
2022-12-08 12:07:41 -0500

    REFACTOR: Deleted Scaffold no we just return Gridview

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -8,22 +8,16 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("DeliMeal"),
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
-      body: GridView(
-        padding: const EdgeInsets.all(25),
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        children: categories
-            .map((e) => CategoryItem(e.id, e.title, e.color))
-            .toList(),
-      ),
+      children:
+          categories.map((e) => CategoryItem(e.id, e.title, e.color)).toList(),
     );
   }
 }
